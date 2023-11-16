ce5abdc4acf3cee7bb4d6192f5b0a109a15ad794
omprakash524 <omprakashpandit524@gmail.com>
2023-11-17 00:33:52 +0530

    my first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>User Management System</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        {{-- css --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+        <style>
+            background-image: url('/img/background.jpg');
+            background-size: cover;
+            background-position: center;
+        </style>
+    </head>
+    <body class="welcome-page">
+        
+        <div class="container-fluid text-center heading-custom">
+            <h1 class="display-4">User Management System</h1>
+            <a class="btn btn-primary" href="{{ route('register') }}">User Registration</a>
+            <a class="btn btn-secondary " href="{{ route('login') }}">User/Admin Login</a>
+        </div>
+
+        {{-- js --}}
+        <link rel="stylesheet" href="{{ asset('js/app.js') }}">
+        <link rel="stylesheet" href="{{ asset('js/style.js') }}">
+    </body>
+</html>
